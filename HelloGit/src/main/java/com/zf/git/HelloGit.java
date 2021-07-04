bc8d9774f23f0559f6b436c1445f26ee6883c001
zhengfan123 <1328704570@qq.com>
2021-07-04 14:55:36 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/zf/git/HelloGit.java
@@ -0,0 +1,13 @@
+package com.zf.git;
+
+/**
+ * @Auther: zf
+ * @Date: 2021/7/4
+ * @Description: com.zf.git
+ * @version: 1.0
+ **/
+public class HelloGit {
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+	}
+}
